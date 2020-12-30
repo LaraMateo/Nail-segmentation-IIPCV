e5dea78d7d78227380f611637b2b376e069083b9
“[Mateo <[lmateo@uninorte.edu.co]>
2020-12-30 09:02:57 +0100

    Implementation of HSV thresholding for H componnent and documentation

--- NailSegmentation.py
+++ NailSegmentation.py
@@ -18,7 +18,6 @@ from  matplotlib import pyplot as plt
 import numpy as np
 import os
 import math
-import time
 
 def FindColors(img, min_U, max_U, min_V, max_V, kernel_size):
     """
@@ -66,10 +65,10 @@ def FindColors(img, min_U, max_U, min_V, max_V, kernel_size):
     blackAndWhiteImage_Areas = blackAndWhiteImage_UV.sum()/255
     if (blackAndWhiteImage_Areas == 0):
         ratio = 22
-        print("Inf")
+        # print("Inf")
     else:
         ratio = int(round(img_Area/blackAndWhiteImage_Areas))
-        print(ratio)
+        # print(ratio)
     if(ratio <= 21):
         #Morphological operations
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernel_size,kernel_size))
@@ -79,7 +78,7 @@ def FindColors(img, min_U, max_U, min_V, max_V, kernel_size):
     else:
         return(np.full((img.shape[0],img.shape[1]),255).astype(np.uint8))
  
-def ConnectedComponents(blackAndWhiteMask, status = "above mean"):
+def ConnectedComponents(blackAndWhiteMask, status = "above mean", min_size = 0, max_size = 0):
     """
     
     For a given mask the connected components that meet certain criteria 
@@ -128,53 +127,22 @@ def ConnectedComponents(blackAndWhiteMask, status = "above mean"):
         max_size =  Non_Background_Stats.mean()
         img = Below(img, max_size)
     else:
-        pass 
+         min_size = Non_Background_Stats.mean()/2
+         img_min = Above(img, min_size)
+         img_max = Below(img, max_size)
+         img = cv2.bitwise_or(img_min, img_max)  
+        
+        
     return(img)
 
 directory = os.fsencode('images')
 for count,file in enumerate(os.listdir(directory)):
     filename = os.fsdecode(file)
-    
-    # filename = 'd6303a64-db67-11e8-9658-0242ac1c0002.jpg'
     img_in = cv2.imread('images/'+filename, 1)
-    gray = cv2.cvtColor(img_in, cv2.COLOR_BGR2GRAY)
+    label = cv2.imread('labels/'+filename, 0)
     # cv2.imshow('Image', img)
     img = cv2.cvtColor(img_in, cv2.COLOR_BGR2YUV)
-    # create a CLAHE object (Arguments are optional) -> Adaptive Histogram Equalization
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    gray = clahe.apply(gray)
-    img[:,:,0] = clahe.apply(img[:,:,0]) # Luminance Y channel
-    # cv2.imshow('Equalized Image', img)
-    
-    img_Area = img.shape[0]*img.shape[1]
-    img_x_size = img.shape[1]
-    img_y_size = img.shape[0]
-    #print("img_Area: ", img_Area)
-    
-    #Convolutions (Image Filtering) and kernel  -> Averaging
-    #blur = cv2.blur(img,(10,10))
-    blur = cv2.cvtColor(img_in, cv2.COLOR_BGR2LAB)  
-    #blur[:,:,2] = clahe.apply(blur[:,:,2]) 
-    blur = cv2.bilateralFilter(blur,5,51,51) #Bilateral Filtering
-      
-    #Edge detection
-    min_threshold = 0.1 * blur.mean()
-    max_threshold = 0.6 * blur.mean()
-    edges = cv2.Canny(blur, 0, max_threshold, apertureSize = 3)
-    #cv2.imshow('Edges', edges)
-    #cv2.imwrite('Results/Edges'+str(count)+'.jpg',edges)  
-    
-    #Histogram
-    # plt.figure(0)
-    # color = ('b','g','r') # b==y g==u and r==v
-    # for i,col in enumerate(color):
-    #     histr = cv2.calcHist([img],[i],None,[256],[0,256])
-    #     plt.plot(histr,color = col)
-    #     plt.xlim([0,256])
-    # plt.xticks(np.arange(256, step=10))
-    # plt.show()
-    # hist = cv2.calcHist([gray],[0],None,[256],[0,256])
-    # plt.hist(gray.ravel(),256,[0,256])
+    # create a CLAHE object (Arguments are optional) -> Adaptive Histogram Equalization 
     
     min_U = 90
     max_U = 125
@@ -185,87 +153,37 @@ for count,file in enumerate(os.listdir(directory)):
     color_clusters =  FindColors(img, min_U, max_U, min_V, max_V, 100)
     
     #find all your connected components above averange
-    img2 = ConnectedComponents(color_clusters, "above max")
-    
+    img2 = ConnectedComponents(color_clusters.copy(), "above max")
+    img2_Areas = img2.sum()/255
     # img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    im_out = cv2.bitwise_and(img, img, mask = img2)
-    #cv2.imwrite('Results/ImageF'+str(count)+'.jpg',im_out)  
-    # cv2.imshow('img Out', im_out)
-    
-    
-    #Histogram
-    # plt.figure(1)
-    color = ('b','g','r')
-    max_values = []
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([im_out],[i],None,[256],[10,256])
-        max_values.append(histr.argmax())
-    #     plt.plot(histr,color = col)
-    #     plt.xlim([1,256])
-    # plt.xticks(np.arange(256, step=10))
-    # plt.show()
-    
-    
-    # #Morphological Transformations for edge
-    # edges = cv2.bitwise_and(edges, img2)
-    # edge_thickness = int(blackAndWhiteImage_Areas*0.002/100)+1
-    # print(edge_thickness)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(edge_thickness,edge_thickness))
-    # edge_dilated = cv2.dilate(edges, kernel)   
-    # # cv2.imshow('Edge dilatated', edge_dilated)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
-    # edge_delimiter = cv2.dilate(edges, kernel)
-    
-    # #(I XOR E) and I
-    # I_xor_E = cv2.bitwise_xor(img2, edge_dilated)
-    # regions = cv2.bitwise_and(I_xor_E, img2)
-    
-    # im_out = cv2.bitwise_and(img_in, img_in, mask = regions)
+    img = cv2.cvtColor(img_in, cv2.COLOR_BGR2HSV)
+    im_out = cv2.bitwise_and(img, img, mask = img2)  
+    #cv2.imwrite('Results/Image'+str(count)+'.jpg',im_out)  
+    # cv2.imshow('img Out', im_out)   
      
+    # Otsu's thresholding 
+    ret,th = cv2.threshold(im_out[:,:,0].astype(np.uint8),10,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th_Area = th.sum()/255
+    if (img2_Areas/2 > th_Area):
+        pass
+    else:
+        th = cv2.bitwise_xor(img2, th)  
+        th_Area = th.sum()/255
     
-    # calculate moments of binary image
-    M = cv2.moments(img2) 
-    # calculate x,y coordinate of center   
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
+    img3 = ConnectedComponents(th.copy(), "other", 0, img2_Areas/28)
     
-    bestX = cX
-    bestY = cY
-    for x in range(100, img_x_size - 100):
-        actual_U = abs(im_out[cY,x,1] - max_values[1])
-        best_U = abs(im_out[bestY, bestX, 1] -max_values[1])
-        actual_V = abs(im_out[cY,x,2] - max_values[2])
-        best_V = abs(im_out[bestY, bestX, 2] -max_values[2])
-        if (actual_U < best_U and actual_V < best_V):
-            bestX = x
-    for y in range(100, img_y_size - 100):
-        actual_U = abs(im_out[y,cX,1] - max_values[1])
-        best_U = abs(im_out[bestY, bestX, 1] -max_values[1])
-        actual_V = abs(im_out[y,cX,2] - max_values[2])
-        best_V = abs(im_out[bestY, bestX, 2] -max_values[2])
-        if (actual_U < best_U and actual_V < best_V):
-            bestY = y
-            bestX = cX
+    cv2.imwrite('Results/Image'+str(count)+'.jpg', img3) 
+    cv2.imwrite('Results/Image-'+str(count)+'.jpg', im_out) 
+     
+    label_area =  label.sum()/255
+    img3_area = img3.sum()/255
+    intersection = cv2.bitwise_and(label, img3)  
+    union = cv2.bitwise_or(label, img3)  
+    intersection_area = intersection.sum()/255
+    union_area = union.sum()/255
+    IoU = intersection_area/union_area
+    Dice = (2*intersection_area)/(label_area + img3_area)
+    print(filename,"IoU: ",round(IoU,3)," Dice: ",round(Dice,3))
     
-    #im_out = cv2.bitwise_and(img_in, img_in, mask = img2)
-    # cv2.circle(im_out, (bestX, bestY), 3, (255, 255, 255), -1)
-    # cv2.imwrite('Results/Region'+str(count)+'.jpg',im_out)
-    
-    skin_U = im_out[bestY, bestX, 1]
-    skin_V = im_out[bestY, bestX, 2]
-    
-    min_U = skin_U - 10
-    max_U = skin_U + 10
-    min_V = skin_V - 10
-    max_V = skin_V + 10
-
-    #Find color clusters 
-    color_clusters =  FindColors(img, min_U, max_U, min_V, max_V, 1)   
-    
-    
-    img3 = cv2.bitwise_xor(img2, color_clusters)
-    im_out = cv2.bitwise_and(img_in, img_in, mask = img3)
-    cv2.imwrite('results/Image-'+str(count)+'.jpg',im_out)
     
     
